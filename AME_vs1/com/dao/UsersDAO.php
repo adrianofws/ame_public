4cd7a2d34446219f7cf2195004c449007dfbb6e6
Projeto Ame <gabrielewerton62@gmail.com>
2020-06-03 01:50:25 +0000

    Atualizações

--- AME_vs1/com/dao/UsersDAO.php
+++ AME_vs1/com/dao/UsersDAO.php
@@ -1,34 +1,35 @@
 <?php
 
-include_once(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/projeto-cloves/AME/ame/com/model/User.php');
-include_once(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/projeto-cloves/AME/ame/com/dao/BaseDAO.php');
+include_once(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/ame_public/AME_vs1/com/model/User.php');
+include_once(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/ame_public/AME_vs1/com/dao/BaseDAO.php');
 
 class UsersDAO extends BaseDAO {
     
     public function insert($cpf, $nome, $endereco) {
 
+        var_dump("chamou insert");
+
         $result = null;
 
-        $sql = "INSERT INTO TABELA_DE_CLIENTES (
+        $sql = "INSERT INTO USUARIOS (
                 CPF, 
                 NOME, 
-                ENDERECO_1) VALUES ($cpf, '$nome', '$endereco')";
+                ENDERECO) VALUES ($cpf, '$nome', '$endereco')";
 
-        $conn = oci_connect('SYSTEM', 'admin', 'localhost:1522/XE');
-        
-        if (!$conn) {
+        $conn = new mysqli('localhost','root','','ame');
 
-            $e = oci_error();
-            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-        
-        } else {
-
-            $data = oci_parse($conn, 'SELECT * FROM TABELA_DE_CLIENTES');
-            oci_execute($data);
-
-            return $data;
-        
+        if ($conn -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $conn -> connect_error;
+            exit();
         }
+
+        if ($result = $conn -> query($sql)) {
+            echo "Returned rows are: " . $result -> num_rows;
+            // Free result set
+            $result -> free_result();
+        }
+        
+        $conn -> close();
 
     } 
 
